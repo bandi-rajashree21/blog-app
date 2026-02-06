e7dc67af8b8bd3d44d3bbb8fb907813615082dab
Bandi Rajashree <bandirs2003@gmail.com>
2026-02-06 15:49:12 +0530

    (feat) : create blog added

--- blogproject/blog/urls.py
+++ blogproject/blog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path('',views.home,name='home'),
-    path('post/<slug:slug>/', views.post_detail, name='post_detail')
+    path('post/<slug:slug>/', views.post_detail, name='post_detail'),
+    path('create/', views.create_post, name='create_post'),
+
 ]
--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Post
+from django.shortcuts import render, redirect
+from .forms import PostForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def home(request):
@@ -10,3 +13,18 @@ def home(request):
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+
+@login_required
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('home')
+    else:
+        form = PostForm()
+
+    return render(request, 'blog/create_post.html', {'form': form})
