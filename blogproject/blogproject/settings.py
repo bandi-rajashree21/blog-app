b6c615ae1080b826159fdd59932bf0afc935e55b
Bandi Rajashree <bandirs2003@gmail.com>
2026-02-06 18:45:26 +0530

    (feat) : add CSS and modified application

--- blogproject/blogproject/settings.py
+++ blogproject/blogproject/settings.py
@@ -116,3 +116,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Additional locations of static files
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/6.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Authentication URLs
+# Redirect to home page after login/logout
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
